3003c1039aed1606d85afd2196c3d7bf1c057fd7
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-07-23 19:02:28 +0900

    feat: [윤주아] 동물병원 팝업창 생성[RAP-MAP-3]

---
+++ src/map/MapInfoPage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import MapBoardListPage from './boardMapPage/MapBoardListPage';
+
+interface Place {
+  place_name: string;
+  address_name: string;
+  phone: string;
+}
+
+interface MapInfoPageProps {
+  place: Place | null;
+  onClose: () => void;
+}
+
+const MapInfoPage: React.FC<MapInfoPageProps> = ({ place, onClose }) => {
+  if (!place) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div style={{ padding: '20px', border: '1px solid #ccc', backgroundColor: '#fff' }}>
+        <h2>{place.place_name}</h2>
+        <p>주소: {place.address_name}</p>
+        <p>전화번호: {place.phone}</p>
+      </div>
+      <MapBoardListPage/>
+      <button onClick={onClose}>닫기</button>
+    </div>
+  );
+};
+
+export default MapInfoPage;
